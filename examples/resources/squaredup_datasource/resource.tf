3d05c1f3a6d615b9e4356a99909d36611b32a049
Shaswot Subedi <shaswot.subedi@squaredup.com>
2023-10-26 16:54:51 +0100

    docs formatting

--- examples/resources/squaredup_datasource/resource.tf
+++ examples/resources/squaredup_datasource/resource.tf
@@ -6,6 +6,7 @@ resource "squaredup_datasource" "sample_data_source" {
   display_name     = "Sample Data"
   data_source_name = data.squaredup_datasources.sample_data.plugins[0].display_name
 }
+
 resource "squaredup_datasource" "ado_datasource" {
   display_name     = "Azure DevOps"
   data_source_name = "Azure DevOps"
