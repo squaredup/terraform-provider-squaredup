611433fc410f58806cf1c4ff49b1cd59b60d299b
Shaswot Subedi <shaswot.subedi@squaredup.com>
2023-11-28 11:01:33 +0545

    test fix

--- internal/provider/data_source_nodes_test.go
+++ internal/provider/data_source_nodes_test.go
@@ -22,15 +22,15 @@ resource "squaredup_datasource" "sample_data_source" {
 	data_source_name = data.squaredup_datasources.sample_data.plugins[0].display_name
 }
 
-data "squaredup_nodes" "acommon_node" {
-	depends_on = [ squaredup_datasource.sample_data_source ]
+data "squaredup_nodes" "acc_common_node" {
+	depends_on     = [squaredup_datasource.sample_data_source]
 	data_source_id = squaredup_datasource.sample_data_source.id
 	node_name      = "account-common-lambda"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.squaredup_nodes.sample_data", "node_properties.0.id"),
-					resource.TestCheckResourceAttr("data.squaredup_nodes.sample_data", "node_properties.0.display_name", "account-common-lambda"),
+					resource.TestCheckResourceAttrSet("data.squaredup_nodes.acc_common_node", "node_properties.0.id"),
+					resource.TestCheckResourceAttr("data.squaredup_nodes.acc_common_node", "node_properties.0.display_name", "account-common-lambda"),
 				),
 			},
 		},
