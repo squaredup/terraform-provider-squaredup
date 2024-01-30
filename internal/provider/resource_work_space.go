4ee2f1aa2f045b957890e74fed8d1b0941091ed2
Shaswot Subedi <shaswot.subedi@squaredup.com>
2024-01-30 09:27:19 +0000

    Revert "default for workspace OA"
    This reverts commit da3e06702c35fe01fa9f158e00d44806826b3c18.

--- internal/provider/resource_work_space.go
+++ internal/provider/resource_work_space.go
@@ -10,7 +10,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
@@ -112,7 +111,6 @@ func (r *workspaceResource) Schema(_ context.Context, req resource.SchemaRequest
 				Description: "Whether open access is enabled for the workspace",
 				Optional:    true,
 				Computed:    true,
-				Default:     booldefault.StaticBool(false),
 			},
 			"id": schema.StringAttribute{
 				Description: "The ID of the workspace",
