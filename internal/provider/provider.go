3eedb09855156216c80e6db2756ce081a8646782
Shaswot Subedi <shaswot.subedi@squaredup.com>
2025-05-07 15:54:06 +0100

    new dashboard ordering resource with test and docs

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -166,5 +166,6 @@ func (p *squaredupProvider) Resources(_ context.Context) []func() resource.Resou
 		SquaredUpScopeResource,
 		SquaredUpDashboardImageResource,
 		SquaredUpDashboardVariableResource,
+		SquaredUpDashboardOrderingResource,
 	}
 }
